0098c5fdeec0582b64e648a47c4e415756db0c1d
ryan m <contact@ryanmeek.com>
2019-03-06 21:02:20 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort{|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  element = array.delete_at(1)
+  array.insert(2, element)
+end
+
+def swap_elements_from_to(array, index, destination)
+  element = array.delete_at(index)
+  array.insert(destination, element)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each{|string| string[2] = "$"}
+end
+
+def find_a(array)
+  array.select{|string| string[0] == "a"}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each{|int| sum = sum + int}
+  sum
+end
+
+def add_s(array)
+  array.each_with_index do |word, index|
+    array[index] = word + "s" if word != array[1]
+  end
+end
